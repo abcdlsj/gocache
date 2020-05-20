df3023c3987cb9541b104bed37c8b50526f99b94
abcdlsj <lisongjianshuai@gmail.com>
2020-05-20 11:53:17 +0800

    complete lru && 通过 groupcache 的 lru_test

---
+++ lru/lru.go
@@ -0,0 +1,104 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	// 最大节点数
+	MaxEntries int
+
+	// 回调函数
+	OnEvicted func(key Key, value Value)
+
+	ll    *list.List
+	cache map[Value]*list.Element
+}
+
+type Key interface{}
+type Value interface{}
+
+type entry struct {
+	key   Key
+	value Value
+}
+
+func New(maxEntries int) *Cache {
+	return &Cache{
+		MaxEntries: maxEntries,
+		ll:         list.New(),
+		cache:      make(map[Value]*list.Element),
+	}
+}
+
+func (c *Cache) Add(key Key, value Value) {
+	if c.cache == nil {
+		c.cache = make(map[Value]*list.Element)
+		c.ll = list.New()
+	}
+	if ee, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ee)
+		ee.Value.(*entry).value = value
+		return
+	}
+	ele := c.ll.PushFront(&entry{key, value})
+	c.cache[key] = ele
+	if c.MaxEntries != 0 && c.ll.Len() > c.MaxEntries {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Get(key Key) (value Value, ok bool) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		c.ll.MoveToFront(ele)
+		return ele.Value.(*entry).value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	if c.cache == nil {
+		return
+	}
+	ele := c.ll.Back()
+	if ele != nil {
+		c.removeElement(ele)
+	}
+}
+
+func (c *Cache) Remove(key Key) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		c.removeElement(ele)
+	}
+}
+
+func (c *Cache) removeElement(e *list.Element) {
+	c.ll.Remove(e)
+	kv := e.Value.(*entry)
+	delete(c.cache, kv.key)
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key, kv.value)
+	}
+}
+
+func (c *Cache) Len() int {
+	if c.cache == nil {
+		return 0
+	}
+	return c.ll.Len()
+}
+
+func (c *Cache) Clear() {
+	if c.OnEvicted != nil {
+		for _, e := range c.cache {
+			kv := e.Value.(*entry)
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+	c.ll = nil
+	c.cache = nil
+}
